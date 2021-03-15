3c9e860cca82fe4a06f131bac9271fa66a24d635
celestial-applet-1261 <zihan991@gmail.com>
2021-03-15 15:11:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function takeANumber (katzDeliLine, name){
 function nowServing (katzDeliLine) {
 
  if (katzDeliLine.length > 0){
-   return `Currently serving ${katzDeliLine.shift()}.`;
+   return `Currently serving ${katzDeliLine.length.shift()}.`;
 } else {
   return `There is nobody waiting to be served!`;
 }
